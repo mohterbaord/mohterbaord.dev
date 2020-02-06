c09918f289b4a06b788760b4e311af16045e298b
Sergey Sokolov <mohterbaord@gmail.com>
2020-02-06 03:44:14 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    {{yearsOfLife}}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class App extends Vue {
+
+  private get yearsOfLife(): string {
+    return '2019\u20132020';
+  }
+
+}
+</script>
